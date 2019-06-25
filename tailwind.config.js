869a9e743ca61b52e45dd148b4fe732c87263e7c
Michael Zuercher <michzuerch@gmail.com>
2019-06-25 14:51:06 +0200

    Renamed original index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+    // Some useful comment
+    extend: {
+      colors: {
+        'primary': '#3b7977',
+        'secondary': '#57a99a',
+      },
+      height: {
+        '128': '20rem',
+        'half': '50vh',
+        '3/4': '75vh'
+      },
+    },
+    variants: {
+      // Some useful comment
+    },
+    plugins: [
+      // Some useful comment
+    ]
+  }
 }
