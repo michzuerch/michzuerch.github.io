6439d4f13cad57106419f274726d2c3739b96a25
Michael Zuercher <michzuerch@gmail.com>
2019-06-26 14:34:20 +0200

    Shadow on title

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,5 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
-  theme: {
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-    },
-    fontFamily: {
-      display: ['Raleway', 'sans-serif'],
-      body: ['Raleway', 'sans-serif'],
-    },
-    borderWidth: {
-      default: '1px',
-      '0': '0',
-      '2': '2px',
-      '4': '4px',
-    },
-    extend: {
-      colors: {
-        cyan: '#9cdbff',
-      },
-      spacing: {
-        '96': '24rem',
-        '128': '32rem',
-      }
-    }
-  }
+  theme: {},
+  variants: {},
+  plugins: [],
 }
