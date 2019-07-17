f8fa8901b3cf362b358b679dcfc3bcf75e0e3681
michzuerch <michzuerch@gmail.com>
2019-07-17 15:24:38 +0200

    added hover.css

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "@fortawesome/fontawesome-free/js/fontawesome";
 import "@fortawesome/fontawesome-free/js/solid";
 import "@fortawesome/fontawesome-free/js/regular";
 import "@fortawesome/fontawesome-free/js/brands";
+import "hover.css/css/hover-min.css";
 
 $(document).ready(function() {
   var owl = $(".owl-carousel");
