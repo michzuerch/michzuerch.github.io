2a294c470ca5ac9d45260c24c10e705cc3da5956
Michael Zuercher <michzuerch@gmail.com>
2019-07-30 11:18:16 +0200

    initial

--- src/index.js
+++ src/index.js
@@ -36,48 +36,4 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     });
   }
-  const owl = $('.owl-carousel');
-  owl.owlCarousel({
-    loop: true,
-    nav: true,
-    autoHeight: true,
-    animateOut: 'slideOutDown',
-    animateIn: 'flipInX',
-    margin: 10,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      600: {
-        items: 1,
-      },
-      960: {
-        items: 1,
-      },
-      1200: {
-        items: 1,
-      },
-    },
-  });
-
-  /* mouse wheel navigation */
-  owl.on('mousewheel', '.owl-stage', function(e) {
-    if (e.deltaY > 0) {
-      owl.trigger('next.owl');
-    } else {
-      owl.trigger('prev.owl');
-    }
-    e.preventDefault();
-  });
-
-  /* keyboard navigation */
-  $(document.documentElement).keyup(function(event) {
-    if (event.keyCode === 37) {
-      /* left key */
-      owl.trigger('prev.owl.carousel', [700]);
-    } else if (event.keyCode === 39) {
-      /* right key */
-      owl.trigger('next.owl.carousel', [700]);
-    }
-  });
 });
