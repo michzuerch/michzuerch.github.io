48bf6d58e7ce1aa759a1aed4ecaa0de370733c0d
Michael Zuercher <michzuerch@gmail.com>
2019-08-08 13:56:04 +0200

    barebone

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app dark height="120" class="orange">
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Portfolio</span>
-        <span class="font-weight-light">Michael Zürcher</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
+     <v-content>
       <HelloWorld/>
     </v-content>
   </v-app>
