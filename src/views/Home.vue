73f8e7105fcf5fce91af4b3581342ee22a9af827
Michael Zuercher <michzuerch@gmail.com>
2019-08-08 14:52:38 +0200

    Button with icons material design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,19 @@
 <div class="home">
     <h1>Homepage</h1>
     <p class="red white--text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo numquam ea natus, velit deleniti nam vel vero dolores eaque architecto totam est? Obcaecati expedita explicabo autem rem maiores doloribus nulla, repudiandae necessitatibus libero. Neque aliquid dolor itaque doloremque saepe rem consectetur exercitationem libero quod quidem, cupiditate rerum quisquam fugit eius, qui architecto blanditiis nisi repudiandae excepturi voluptatibus ipsum aut quo impedit. Molestias laboriosam iure ipsa ea fugiat. Veniam consequuntur nulla harum pariatur rem, illo veritatis porro quos sit soluta fuga.</p>
-    <p class="pink lighten-4 red--text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic consequuntur natus eligendi cupiditate veritatis, quisquam nihil ipsa odio perferendis iste quos eum sapiente ea, fugiat quis. Nostrum aliquam tempore similique fugiat veritatis mollitia modi esse nulla deserunt deleniti provident sapiente, impedit minima illum enim nihil velit beatae quae atque est. Doloribus optio, animi itaque temporibus commodi magnam libero neque impedit!</p>
+    <p class="pink lighten-4 red--text text--darken-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic consequuntur natus eligendi cupiditate veritatis, quisquam nihil ipsa odio perferendis iste quos eum sapiente ea, fugiat quis. Nostrum aliquam tempore similique fugiat veritatis mollitia modi esse nulla deserunt deleniti provident sapiente, impedit minima illum enim nihil velit beatae quae atque est. Doloribus optio, animi itaque temporibus commodi magnam libero neque impedit!</p>
+    <h1>Massive text size</h1>
+    <h1 class="display-4">Bigger than before</h1>
+    <h1 class="display-1">One</h1>
+    <v-btn class="pink white--text">Click here</v-btn>
+    <v-btn depressed color="pink">Click here</v-btn>
+    <v-btn flat color="pink">Click here</v-btn>
+    <v-btn depressed class="pink text--white">
+        <v-icon>mdi-android</v-icon>
+    </v-btn>
+    <v-btn text icon color="pink">
+        <v-icon>mdi-mouse</v-icon>
+    </v-btn>
 </div>
 </template>
 
