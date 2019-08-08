8d09fcb1c6c68469a532aa8d77a6c166f3748561
Michael Zuercher <michzuerch@gmail.com>
2019-08-08 14:12:57 +0200

    text color

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,11 @@
 <template>
-  <HelloWorld />
+<div class="home">
+    <h1>Homepage</h1>
+    <p class="red white--text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo numquam ea natus, velit deleniti nam vel vero dolores eaque architecto totam est? Obcaecati expedita explicabo autem rem maiores doloribus nulla, repudiandae necessitatibus libero. Neque aliquid dolor itaque doloremque saepe rem consectetur exercitationem libero quod quidem, cupiditate rerum quisquam fugit eius, qui architecto blanditiis nisi repudiandae excepturi voluptatibus ipsum aut quo impedit. Molestias laboriosam iure ipsa ea fugiat. Veniam consequuntur nulla harum pariatur rem, illo veritatis porro quos sit soluta fuga.</p>
+    <p class="pink lighten-4 red--text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic consequuntur natus eligendi cupiditate veritatis, quisquam nihil ipsa odio perferendis iste quos eum sapiente ea, fugiat quis. Nostrum aliquam tempore similique fugiat veritatis mollitia modi esse nulla deserunt deleniti provident sapiente, impedit minima illum enim nihil velit beatae quae atque est. Doloribus optio, animi itaque temporibus commodi magnam libero neque impedit!</p>
+</div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld';
-
-export default {
-  components: {
-    HelloWorld,
-  },
-};
+export default {};
 </script>
