166e739965660f6fb8f4dfbfef052d7f76ff2baa
Michael Zuercher <michzuerch@gmail.com>
2019-08-13 13:43:24 +0200

    Better gallery

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,10 +1,21 @@
 <template>
 <v-container grid-list-md text-center>
-    <v-layout>
-        <v-carousel>
+    <v-card elevation="24">
+        <v-carousel :cycle="cycle" :interval="interval">
             <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src" reverse-transition="fade-transition" transition="fade-transition"></v-carousel-item>
         </v-carousel>
-    </v-layout>
+        <v-list two-line>
+            <v-list-item>
+                <v-list-item-avatar>
+                    <v-img src="img/Avatar2018.jpg"></v-img>
+                </v-list-item-avatar>
+                <v-list-item-content>
+                    <v-list-item-title>Michael Zürcher</v-list-item-title>
+                    <v-list-item-subtitle>Author</v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
+    </v-card>
 </v-container>
 </template>
 
@@ -12,6 +23,8 @@
 export default {
     data() {
         return {
+            cycle: true,
+            interval: 10000,
             items: [{
                     src: 'img/gallery/Portrait1-cut.jpg',
                 },
