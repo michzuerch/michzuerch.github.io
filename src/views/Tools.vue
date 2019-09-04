9d7976cd472e2b7b1b0cd6ec2227b1117399cddb
michzuerch <michzuerch@gmail.com>
2019-09-04 13:22:29 +0200

    Ready

--- src/views/Tools.vue
+++ src/views/Tools.vue
@@ -1,123 +1,87 @@
 <template>
-<v-container grid-list-xl>
-    <v-layout row justify-space-between>
-        <v-flex md3>
-            <v-card class="ma-2">
-                <v-card-title>Virtualisierung</v-card-title>
+<v-container grid-list-sm fluid>
+    <v-layout row wrap>
+        <v-flex xs4>
+            <v-card flat tile>
+                <v-img src="img/tools/docker.jpg" height="200"></v-img>
                 <v-card-text>
-                    <v-hover v-slot:default="{ hover }">
-                        <v-card class="ma-2" :elevation="hover ? 24 : 2">
-                            <v-img src="img/tools/docker.jpg" height="200"></v-img>
-                            <v-card-text>
-                                <h1>Docker</h1>
-                                <p>Virtualisierung</p>
-                            </v-card-text>
-                        </v-card>
-                    </v-hover>
-                    <v-hover v-slot:default="{ hover }">
-                        <v-card class="ma-2" :elevation="hover ? 24 : 2">
-                            <v-img src="img/tools/openshift.jpg" height="200"></v-img>
-                            <v-card-text>
-                                <h1>Openshift</h1>
-                                <p>Virtualisierung</p>
-                            </v-card-text>
-                        </v-card>
-                    </v-hover>
+                    <h2 class="subtitle-2 font-weight-bold">Docker</h2>
+                    <p class="font-weight-regular">Virtualisierung</p>
                 </v-card-text>
-
-                <v-card class="ma-2">
-                    <v-card-title>Webdesign</v-card-title>
-                    <v-card-text>
-                        <v-hover v-slot:default="{ hover }">
-                            <v-card class="ma-2" :elevation="hover ? 24 : 2">
-                                <v-img src="img/tools/html5css3.jpg" height="200"></v-img>
-                                <v-card-text>
-                                    <h1>Html 5 / CSS 3</h1>
-                                    <p>Webdesign</p>
-                                </v-card-text>
-                            </v-card>
-                        </v-hover>
-                        <v-hover v-slot:default="{ hover }">
-                            <v-card class="ma-2" :elevation="hover ? 24 : 2">
-                                <v-img src="img/tools/vue.jpg" height="200"></v-img>
-                                <v-card-text>
-                                    <h1>Vue JS</h1>
-                                    <p class="pt-1">Javascript Framework</p>
-                                </v-card-text>
-                            </v-card>
-                        </v-hover>
-                        <v-hover v-slot:default="{ hover }">
-                            <v-card class="ma-2" :elevation="hover ? 24 : 2">
-                                <v-img src="img/tools/bulma.jpg" height="200"></v-img>
-                                <v-card-text>
-                                    <h1>Bulma</h1>
-                                    <p>CSS3-Framework</p>
-                                </v-card-text>
-                            </v-card>
-                        </v-hover>
-                        <v-hover v-slot:default="{ hover }">
-                            <v-card class="ma-2" :elevation="hover ? 24 : 2">
-                                <v-img src="img/tools/tailwind.jpg" height="200"></v-img>
-                                <v-card-text>
-                                    <h1>Tailwind CSS</h1>
-                                    <p class="pt-1">CSS Framework</p>
-                                </v-card-text>
-                            </v-card>
-                        </v-hover>
-                    </v-card-text>
-                </v-card>
-
-                <v-card class="ma-2">
-                    <v-card-title>Programmierung</v-card-title>
-                    <v-card-text>
-                        <v-hover v-slot:default="{ hover }">
-                            <v-card class="ma-2" :elevation="hover ? 24 : 2">
-                                <v-img src="img/tools/intellij.jpg" height="200"></v-img>
-                                <v-card-text>
-                                    <h1>IntelliJ </h1>
-                                    <p>IDE Java</p>
-                                </v-card-text>
-                            </v-card>
-                        </v-hover>
-                        <v-hover v-slot:default="{ hover }">
-                            <v-card class="ma-2" :elevation="hover ? 24 : 2">
-                                <v-img src="img/tools/java.jpg" height="200"></v-img>
-                                <v-card-text>
-                                    <h1>Java 12</h1>
-                                    <p>Programmierung</p>
-                                </v-card-text>
-                            </v-card>
-                        </v-hover>
-                        <v-hover v-slot:default="{ hover }">
-                            <v-card class="ma-2" :elevation="hover ? 24 : 2">
-                                <v-img src="img/tools/maven.jpg" height="200"></v-img>
-                                <v-card-text>
-                                    <h1>Maven</h1>
-                                    <p>Buildtool</p>
-                                </v-card-text>
-                            </v-card>
-                        </v-hover>
-                        <v-hover v-slot:default="{ hover }">
-                            <v-card class="ma-2" :elevation="hover ? 24 : 2">
-                                <v-img src="img/tools/spring-boot.jpg" height="200"></v-img>
-                                <v-card-text>
-                                    <h1>Spring Boot</h1>
-                                    <p class="pt-1">Java Framework</p>
-                                </v-card-text>
-                            </v-card>
-                        </v-hover>
-                        <v-hover v-slot:default="{ hover }">
-                            <v-card class="ma-2" :elevation="hover ? 24 : 2">
-                                <v-img src="img/tools/javascript.jpg" height="200"></v-img>
-                                <v-card-text>
-                                    <h1>Javascript</h1>
-                                    <p class="pt-1">Frontend Programming</p>
-                                </v-card-text>
-                            </v-card>
-                        </v-hover>
-                    </v-card-text>
-                </v-card>
             </v-card>
+        </v-flex>
+        <v-flex xs4>
+            <v-card flat tile>
+                <v-img src="img/tools/openshift.jpg" height="200"></v-img>
+                <v-card-text>
+                    <h2 class="subtitle-2 font-weight-bold">Openshift</h2>
+                    <p class="font-weight-regular">Virtualisierung</p>
+                </v-card-text>
+            </v-card>
+        </v-flex>
+        <v-flex xs4>
+            <v-card flat tile>
+                <v-img src="img/tools/html5css3.jpg" height="200"></v-img>
+                <v-card-text>
+                    <h2 class="subtitle-2 font-weight-bold">HTML5,CSS3,SASS,Tailwind,Bulma</h2>
+                    <p class="font-weight-regular">Webdesign</p>
+                </v-card-text>
+            </v-card>
+        </v-flex>
+        <v-flex xs4>
+            <v-card flat tile>
+                <v-img src="img/tools/vue.jpg" height="200"></v-img>
+                <v-card-text>
+                    <h2 class="subtitle-2 font-weight-bold">Vue,Vuetify</h2>
+                    <p class="font-weight-regular">Frontend Programmierung</p>
+                </v-card-text>
+            </v-card>
+        </v-flex>
+        <v-flex xs4>
+            <v-card flat tile>
+                <v-img src="img/tools/java.jpg" height="200"></v-img>
+                <v-card-text>
+                    <h2 class="subtitle-2 font-weight-bold">Java 12</h2>
+                    <p class="font-weight-regular">Java Programmierung</p>
+                </v-card-text>
+            </v-card>
+        </v-flex>
+        <v-flex xs4>
+            <v-card flat tile>
+                <v-img src="img/tools/intellij.jpg" height="200"></v-img>
+                <v-card-text>
+                    <h2 class="subtitle-2 font-weight-bold">IntelliJ</h2>
+                    <p class="font-weight-regular">Java IDE</p>
+                </v-card-text>
+            </v-card>
+        </v-flex>
+        <v-flex xs4>
+            <v-card flat tile>
+                <v-img src="img/tools/maven.jpg" height="200"></v-img>
+                <v-card-text>
+                    <h2 class="subtitle-2 font-weight-bold">Maven</h2>
+                    <p class="font-weight-regular">Java Buildtool</p>
+                </v-card-text>
+            </v-card>
+        </v-flex>
+        <v-flex xs4>
+            <v-card flat tile>
+                <v-img src="img/tools/spring-boot.jpg" height="200"></v-img>
+                <v-card-text>
+                    <h2 class="subtitle-2 font-weight-bold">Spring-Boot</h2>
+                    <p class="font-weight-regular">Java Framework</p>
+                </v-card-text>
+            </v-card>
+        </v-flex>
+        <v-flex xs4>
+            <v-card flat tile>
+                <v-img src="img/tools/javascript.jpg" height="200"></v-img>
+                <v-card-text>
+                    <h2 class="subtitle-2 font-weight-bold">Javascript,ECMAScript</h2>
+                    <p class="font-weight-regular">Frontend Programmierung</p>
+                </v-card-text>
+            </v-card>
+        </v-flex>
     </v-layout>
 </v-container>
 </template>
