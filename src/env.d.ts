a779d31f7d08b1f3af8ef118c62447defa9550b9
Michael Zuercher <michzuerch@gmail.com>
2023-12-09 18:22:04 +0100

    initial from template

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
-/// <reference types="@astrojs/image/client" />
-declare module 'hero-patterns'
-declare module 'lunr'
-declare module 'photoswipe-dynamic-caption-plugin'
+/// <reference types="astro/client" />
