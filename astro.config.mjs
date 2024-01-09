b68313c56af6f832f8d6f6185ffc8990f72d2129
Michael Zuercher <michzuerch@gmail.com>
2024-01-09 14:23:10 +0100

    move to bun, sample with figlet in index.ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import icon from 'astro-icon'
 
-import icon from "astro-icon";
+import alpinejs from '@astrojs/alpinejs'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
-});
+  integrations: [
+    tailwind(),
+    icon({
+      include: {
+        mdi: ['*'], // (Default) Loads entire Material Design Icon set
+      },
+    }),
+    alpinejs(),
+  ],
+})
