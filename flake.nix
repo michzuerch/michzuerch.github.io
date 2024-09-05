fee724e6a0002ec0f8e8f624c49205be465374fa
Michael Zuercher <michzuerch@gmail.com>
2024-09-05 12:36:32 +0200

    reformat flake.nix

--- flake.nix
+++ flake.nix
@@ -1,28 +1,5 @@
 {
-  description = "Blog Playwright NixOS webdev";
-
-  outputs = inputs@{ self, nixpkgs, utils, devToolkit, haumea, ... }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-
-        src = haumea.lib.load {
-          src = ./.nix;
-          inputs = { inherit pkgs; };
-        };
-      in {
-        devShell = devToolkit.lib.${system}.buildDevShell {
-          name = "blog.playwright-nixos-webdev";
-          profiles = [ "node" ];
-          extraPackages = [ src.playwright-browsers-1_46_1 ];
-          extraShellHook = ''
-            # Prepare playwright
-            export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
-            export PLAYWRIGHT_BROWSERS_PATH=${src.playwright-browsers-1_46_1}
-            export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
-          '';
-        };
-      });
+  description = "Blog Playwright michzuerch.github.io";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -38,4 +15,28 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs = inputs@{ self, nixpkgs, utils, devToolkit, haumea, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        src = haumea.lib.load {
+          src = ./.nix;
+          inputs = { inherit pkgs; };
+        };
+      in {
+        devShell = devToolkit.lib.${system}.buildDevShell {
+          name = "michzuerch webdev";
+          profiles = [ "node" ];
+          extraPackages = [ src.playwright-browsers-1_46_1 ];
+          extraShellHook = ''
+            # Prepare playwright
+            export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
+            export PLAYWRIGHT_BROWSERS_PATH=${src.playwright-browsers-1_46_1}
+            export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
+          '';
+        };
+      });
+
 }
